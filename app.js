80b92dd4410ee6831966c11abfdd5fce3d3f5c0d
stefnotadevyet <srjack91@gmail.com>
2021-10-26 22:04:37 -0400

    changed form to button to test fetch method

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const api = "xlWsYUzC4pXNjlBmCdSzNN909eCAvE82LP9HWJKV"
-const url = `https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token=${api}`
 
 const handleSubmit = () => {
-
+  // event.preventDefault();
+  const display = document.getElementsByClassName('display')
+  const api = "xlWsYUzC4pXNjlBmCdSzNN909eCAvE82LP9HWJKV"
+  let url = `https://api.stockdata.org/v1/data/intraday?symbols=AAPL&api_token=${api}`
 
 
   fetch(url).then((res) => res.json())
-            .then((data) => console.log(data))
+            .then((data) => {
+              console.log(data)
+            })
+            .catch((err) => console.error(err))
+
 
 
 }
